e1228e2f9bfd07da55de51dc87fcef945139b80d
EC2 Default User <ec2-user@ip-172-31-33-33.ec2.internal>
2021-03-13 08:14:21 +0000

    [Edit]投稿一覧ページのレイアウト及び新規投稿機能実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,31 @@
 class BooksController < ApplicationController
+    
+    def index
+        @books = Book.all
+        @book = Book.new
+    end
+        
+    def new
+        # @book = Book.new
+    end
+    
+    def create
+       book = Book.new(book_params)
+       book.save
+       redirect_to book_path(book)
+    end
+    
+    def show
+        
+    end
+    
+    
+    
+    private
+    
+    def book_params
+       params.require(:book).permit(:title, :body) 
+    end
+        
+    
 end
