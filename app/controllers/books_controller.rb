d2c7b9609d47047ddcf4d142bc1f6f23c8ce3723
EC2 Default User <ec2-user@ip-172-31-33-33.ec2.internal>
2021-03-13 09:08:13 +0000

    [Add]show, edit, update, destroyアクションの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,7 +16,23 @@ class BooksController < ApplicationController
     end
     
     def show
-        
+       @book = Book.find(params[:id]) 
+    end
+    
+    def edit
+       @book = Book.find(params[:id])
+    end
+    
+    def update
+       book = Book.find(params[:id])
+       book.update(book_params)
+       redirect_to book_path
+    end
+    
+    def destroy
+       book = Book.find(params[:id])
+       book.destroy
+       redirect_to books_path
     end
     
     
